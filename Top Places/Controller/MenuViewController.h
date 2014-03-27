1999d07536ad513be016f2fab9ab198049055d40
acicartagena <acicartagena@gmail.com>
2014-03-27 17:04:09 +0800

    iPad version. still not fixed

---
+++ Top Places/Controller/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Top Places
+//
+//  Created by Angela Cartagena on 3/27/14.
+//  Copyright (c) 2014 acicartagena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UITableViewController
+
+@end
---
+++ Top Places/Controller/MenuViewController.m
@@ -0,0 +1,26 @@
+//
+//  MenuViewController.m
+//  Top Places
+//
+//  Created by Angela Cartagena on 3/27/14.
+//  Copyright (c) 2014 acicartagena. All rights reserved.
+//
+
+#import "MenuViewController.h"
+
+@interface MenuViewController ()
+
+@end
+
+@implementation MenuViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"Menu";
+}
+
+
+
+@end
